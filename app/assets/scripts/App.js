44cb8f39f4decb433586a000d5bec1451c2db69d
Odd Olsen <odd.olsen@live.se>
2017-03-24 10:22:22 +0100

    JavaScript test

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,3 @@
-var $ = require('jquery');
-// var Person = require('./modules/Person');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-    payTaxes() {
-        console.log(this.name+" now owns 0$ in tax");
-    }
-}
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet(); 
-jane.payTaxes();
-
-// $("h1").remove();
+var mobileMenu = new MobileMenu();
