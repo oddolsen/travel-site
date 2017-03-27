5c6642de60b03aa317f298467f95c32b91ff59ea
Odd Olsen <odd.olsen@live.se>
2017-03-27 13:29:14 +0200

    Menu icon

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -107,6 +107,7 @@ var MobilMenu = function () {
         value: function toggleTheMenu() {
             this.menuContent.toggleClass("site-header__menu-content--is-visible");
             this.siteHeader.toggleClass("site-header--is-expanded");
+            this.menuIcon.toggleClass("site-header__menu-icon--close-x");
             console.log("Running!!");
         }
     }]);
